4f615a3879199e69951ccdfba1aa703282634af6
Carol Souza <caah.souza@live.com>
2020-10-18 00:40:26 -0300

    primeiro dia

---
+++ web/src/App.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { FiArrowRight } from 'react-icons/fi';
+
+import './styles/global.css';
+
+import Routes from './routes';
+
+/* funcionamento typescript
+interface TitleProps{
+  text: string;
+}
+
+function Title(props: TitleProps){
+  return(
+    <h1>{props.text}</h1>
+  )
+}*/
+
+function App() {
+  return (
+    <Routes />
+  );
+}
+
+export default App;
